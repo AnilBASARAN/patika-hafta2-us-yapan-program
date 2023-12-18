437b985d60ec0f07b7d5702ad8ce250a6918fab6
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2023-12-18 05:59:53 +0300

    üs alma java programı
    Methodları kullanarak üs alma programı yazıyoruz

---
+++ UsAlma.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class UsAlma {
+
+    // UsAlma sınıfında usAlma isminde bir method belirledik.
+    // bu metod statik olmadığı için main methodunun içinde yeni bir obje oluşturduk ve
+    // bu objenin  sayesinde usAlme methodunu çağırdık
+    public int  usAlma(int a, int b){
+        int result = 1;
+    for(int i = 1;i <=b;i++){
+        result *= a;
+        }
+    return result;
+    }
+
+    public static void main(String[] args) {
+        int base,exponential;
+        Scanner input = new Scanner(System.in);
+
+        // base ve exponential degiskenlerinde saklıyoruz kullanıcıdan aldığımız değerleri
+
+        System.out.print("Üssü alınacak sayıyı girin:   ");
+        base = input.nextInt();
+
+        System.out.print("Üssü girin:   ");
+        exponential = input.nextInt();
+
+        UsAlma sonuc = new UsAlma();
+        int result = sonuc.usAlma(base,exponential);
+
+        System.out.print(base+" üzeri "+exponential+" :  "+result);
+
+    }
+}
